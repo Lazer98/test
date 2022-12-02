488204ed118a20801c1fbf22d600ff51d02dd20e
Lazer98 <96163637+Lazer98@users.noreply.github.com>
2022-12-02 10:06:34 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import AppStart from './start_compose/appStart';
+
+function App() {
+
+  let user = "koko";
+  let num1 = 5;
+  let num2 = 8;
+
+  return (
+    <div className="App">
+      <AppStart />
+      <hr/>
+      <h1 className='text-danger'>Welcome to react!</h1>
+     </div>
+
+  
+)
+}
+
+export default App;
---
+++ start_compose/appStart.js
@@ -0,0 +1,11 @@
+// rfc - קיצור ליצירת קומפנינטה
+import React from 'react'
+
+
+export default function AppStart(props) {
+  return (
+    <div className='container'>
+ 
+    </div>
+  )
+}
